0940840dc5172bc237addd0e10604a0bf6fccfd3
Fernando Rocha <fernandorff92@gmail.com>
2022-10-07 06:14:32 -0300

    123

--- Algoritmo2/encontraMenores.js
+++ Algoritmo2/encontraMenores.js
@@ -1,16 +1,44 @@
 const listaLivros = require("./array");
 
-function encontraMenores(pivot, array) {
+function encontraMenores(pivo, array) {
   let menores = 0;
 
   for (let atual = 0; atual < array.length; atual++) {
     let produtoAtual = array[atual];
-    if (produtoAtual.preco < pivot.preco) {
+    if (produtoAtual.preco < pivo.preco) {
       menores++;
     }
   }
 
-  return menores;
+  trocaLugar(array, array.indexOf(pivo), menores);
+  return array;
 }
 
-console.log(encontraMenores(listaLivros[2], listaLivros));
+function trocaLugar(array, de, para) {
+  const elem1 = array[de];
+  const elem2 = array[para];
+
+  array[para] = elem1;
+  array[de] = elem2;
+}
+
+function divideNoPivo(array) {
+  let pivo = array[Math.floor(array.length / 2)];
+  encontraMenores(pivo, array);
+  let valoresMenores = 0;
+
+  for (let analisando = 0; analisando < array.length; analisando++) {
+    let atual = array[analisando];
+    if (atual.preco <= pivo.preco && atual !== pivo) {
+      trocaLugar(array, analisando, valoresMenores);
+      valoresMenores++;
+    }
+  }
+
+  return array;
+}
+
+// console.log(divideNoPivo(listaLivros));
+// console.log(encontraMenores(listaLivros[2], listaLivros));
+
+module.exports = trocaLugar;
